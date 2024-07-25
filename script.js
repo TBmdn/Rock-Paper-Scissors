18f9056c925f3353b7f6c31d3702c0a511869fca
TBmdn <tboumeddine@gmail.com>
2024-07-25 07:03:40 +0100

    code getComputerChoice function
    this function generate the computer choice

---
+++ script.js
@@ -0,0 +1,15 @@
+function getComputerChoice(){
+    let randomChoice = Math.floor(Math.random() * 3);
+
+    switch (randomChoice){
+        case 0:
+            return 'rock'
+            break;
+        case 1:
+            return 'paper'
+            break;
+        case 2:
+            return 'scissor'
+            break;
+    }
+}
