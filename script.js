1675e720447006001e8902221ed735dbf6edac20
TBmdn <tboumeddine@gmail.com>
2024-07-25 11:26:03 +0100

    code scoreBoard,youLose,youWin,playRound functions
    scoreBoard is a function to print the score of the two players
    youLose is a function to print a message when the user lose
    youWin is a function to print a message when the user wins
    playRound is a function to play one round of the game and increament the score counter of the players
    
    in addition to fix some bugs in the previous functions

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ function getComputerChoice(){
 
     switch (randomChoice){
         case 0:
+            console.log(`Computer : ROCK`);
             return 'ROCK';
             break;
         case 1:
+            console.log(`Computer : PAPER`);
             return 'PAPER';
             break;
         case 2:
-            return 'SCISSORS';
+            console.log(`Computer : SCISSOR`);
+            return 'SCISSOR';
             break;
     }
 }
@@ -19,15 +22,80 @@ function getHumanChoice(){
 
     switch(choice){
         case 'ROCK':
+            console.log(`You : ROCK`);
             return choice;
             break;
         case 'PAPER':
+            console.log(`You : PAPER`);
             return choice;
             break;
         case 'SCISSOR':
+            console.log(`You : SCISSOR`);
             return choice;
             break;
         default:
             getHumanChoice();
     }
 }
+
+function scoreBoard(){
+    console.log(`Your score: ${humanScore}, Computer score: ${computerScore}`);
+}
+
+function youLose(winnerChoice,loserChoice){
+    console.log(`You lose!, ${winnerChoice} beats ${loserChoice}`);
+}
+function youWin(winnerChoice,loserChoice){
+    console.log(`You Win!, ${winnerChoice} beats ${loserChoice}`);
+}
+
+function playRound(computerChoice,humanChoice){
+    if (computerChoice == humanChoice) {
+        console.log('Draw!');
+        scoreBoard();
+    }
+    else{
+        switch(computerChoice){
+            case 'ROCK':
+                if(humanChoice == 'PAPER'){
+                    ++humanScore;
+                    youWin(humanChoice,computerChoice);
+                    scoreBoard();
+                }
+                else{
+                    ++computerScore;
+                    youLose(computerChoice,humanChoice);
+                    scoreBoard();
+                }
+                break;
+            case 'PAPER':
+                if(humanChoice == 'ROCK'){
+                    ++computerScore;
+                    youLose(computerChoice,humanChoice);
+                    scoreBoard();
+                }
+                else{
+                    ++humanScore;
+                    youWin(humanChoice,computerChoice);
+                    scoreBoard();
+                }
+                break;
+            case 'SCISSOR':
+                if (humanChoice == 'ROCK') {
+                    ++humanScore;
+                    youWin(humanChoice,computerChoice);
+                    scoreBoard();
+                }
+                else{
+                    ++computerScore;
+                    youLose(computerChoice,humanChoice);
+                    scoreBoard();
+                }
+                break;
+        }
+    }
+}
+
+let humanScore = 0,computerScore = 0;
+
+console.log(playRound(getComputerChoice(),getHumanChoice()));
