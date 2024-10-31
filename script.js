4f28afd15483c2702cff2f870024eb376dd7f226
TBmdn <tboumeddine@gmail.com>
2024-10-31 11:05:35 +0100

    completed a prototype UI

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+let humanScore = 0,computerScore = 0;
+const choices = document.getElementById('choices');
+const dashboard = document.getElementById('dashboard');
+const rockBtn = document.getElementById('Rock');
+const paperBtn = document.getElementById('Paper');
+const scissorBtn = document.getElementById('Scissor');
+const userChoicePara = document.getElementById('user-choice');
+const computerChoicePara = document.getElementById('computer-choice');
+const roundResultPara = document.getElementById('round-result');
+const scorePara = document.getElementById('score');
+
 function getComputerChoice(){
+    // generates a random number between 1 and 3 
     let randomChoice = Math.floor(Math.random() * 3);
 
     switch (randomChoice){
@@ -14,45 +26,27 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    let choice = prompt('Enter you choice :').toUpperCase();
-
-    switch(choice){
-        case 'ROCK':
-            return choice;
-            break;
-        case 'PAPER':
-            return choice;
-            break;
-        case 'SCISSOR':
-            return choice;
-            break;
-        default:
-            console.log(`error! enter your choice correctly`);
-            getHumanChoice();
-    }
-}
-
 function scoreBoard(){
-    console.log(`Your score: ${humanScore}, Computer score: ${computerScore}`);
+    scorePara.textContent = `Your score: ${humanScore}, Computer score: ${computerScore}`;
 }
 
 function youLose(winnerChoice,loserChoice){
-    console.log(`You lose!, ${winnerChoice} beats ${loserChoice}`);
+    roundResultPara.textContent = `You lose!, ${winnerChoice} beats ${loserChoice}`;
 }
+
 function youWin(winnerChoice,loserChoice){
-    console.log(`You Win!, ${winnerChoice} beats ${loserChoice}`);
+    roundResultPara.textContent = `You win!, ${winnerChoice} beats ${loserChoice}`;
 }
 
 function printChoices(computerChoice,humanChoice){
-    console.log(`You: ${humanChoice}`);
-    console.log(`Computer: ${computerChoice}`);
+    userChoicePara.textContent = `You :${humanChoice}`;
+    computerChoicePara.textContent = `Computer :${computerChoice}`;
 }
 
 function playRound(computerChoice,humanChoice){
     printChoices(computerChoice,humanChoice);
     if (computerChoice == humanChoice) {
-        console.log('Draw!');
+        roundResultPara.textContent = 'Draw!';
         scoreBoard();
     }
     else{
@@ -95,20 +89,35 @@ function playRound(computerChoice,humanChoice){
                 break;
         }
     }
+    if (humanScore == 5) {
+        choices.setAttribute("style","display: none;");
+        let gameResultPara = document.createElement('p');
+        gameResultPara.textContent = 'Great! You win the game';
+        dashboard.appendChild(gameResultPara);
+        playAgain();
+    }
+    else if (computerScore == 5) {
+        choices.setAttribute("style","display: none;");
+        let gameResultPara = document.createElement('p');
+        gameResultPara.textContent = 'Sorry! You lose the game';
+        dashboard.appendChild(gameResultPara);
+        playAgain();
+    }
 }
 
-function playGame(){
+// a function that add a button to reload the page if the user wants to play another game 
+function playAgain(){
+    let plyAgainBtn = document.createElement('button');
+    let plyAgainPara = document.createElement('p');
+    plyAgainBtn.textContent = 'Play Again';
+    plyAgainPara.textContent = 'let\'s play another game';
+    dashboard.appendChild(plyAgainPara);
+    dashboard.appendChild(plyAgainBtn);
 
-    if (humanScore == computerScore) {
-        console.log(`Draw!`);
-    }
-    else if (humanScore < computerScore) {
-        console.log(`You Lose!`);
-    }
-    else {
-        console.log(`You Win`);
-    }
+    // location.reload is method to reload the page
+    plyAgainBtn.addEventListener('click',() => location.reload());
 }
 
-let humanScore = 0,computerScore = 0;
-console.log(playGame());
+rockBtn.addEventListener("click", () => playRound(getComputerChoice(),"ROCK"));
+paperBtn.addEventListener("click", () => playRound(getComputerChoice(),"PAPER"));
+scissorBtn.addEventListener("click", () => playRound(getComputerChoice(),"SCISSOR"));
